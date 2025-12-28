8b98893af68d303bafbc249edfdfcab0f495e0af
AnayMishra2006 <anaymishra20212105@gmail.com>
2025-12-29 04:05:55 +0530

    Building a simple LLM

---
+++ Building Simple LLM Application using LCEL/serve.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import os 
+from langserve import add_routes
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+model=ChatGroq(model="openai/gpt-oss-120b",groq_api_key=groq_api_key)
+
+#1. Create prompt template
+generic_template="Translate the following into {language}:"
+
+prompt=ChatPromptTemplate.from_messages(
+    [("system",generic_template),("user","{text}")]
+)
+
+parser=StrOutputParser()
+
+## Create chain
+chain=prompt|model|parser
+
+## App definition
+app=FastAPI(title="Langchain Server",
+            version="1.0",
+            description="A simple API server using Langchain runnable inerfaces")
+
+## Adding chain routes
+
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1",port=8000)
